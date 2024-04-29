bf51a7d0284e6c5439e9df5a3a1d143fe8a77eae
CLADAU <168415253+CLADAU@users.noreply.github.com>
2024-04-29 20:16:31 +1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PHmtkNHQ795qH_pIQujfcgJhze0GRlStGvmcH8QWBOI/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDdlaUD_ghXvu50ierpCTwRVzDStZP3iNE';
