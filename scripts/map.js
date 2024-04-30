88155c0a5185108eeedc6bbf687db43f2991eda7
CLADAU <168415253+CLADAU@users.noreply.github.com>
2024-04-30 16:16:59 +1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -947,10 +947,6 @@ $(window).on('load', function() {
       accessToken: trySetting('_tileProviderApiKey', '')
     }).addTo(map);
 
-    L.control.attribution({
-      position: trySetting('_mapAttribution', 'bottomright')
-    }).addTo(map);
-
   }
 
   /**
